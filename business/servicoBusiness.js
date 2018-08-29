055ad3eb0b285bdaf536ad17104f974c27793db1
Prisleey <prisleey.araujo@hotmail.com>
2018-08-28 22:07:28 -0300

    fiz tudo a busca dos estúdios

--- business/servicoBusiness.js
+++ business/servicoBusiness.js
@@ -1,28 +1,71 @@
 let EstudioModel = require('../model/estudioModel');
 
 exports.buscar = function(data) {
+console.log('felipebd');
     return new Promise(function(resolve, reject) {
-        EstudioModel.find({
-            "$or": [{
-                nomeEstudio: {'$regex': data.nomeBusca, '$options': 'i'}
+
+        if(data.nomeBusca.trim() != "" && data.local.trim() != "") {
+
+            EstudioModel.find({
+                "$or": [{
+                    nomeEstudio: {'$regex': data.nomeBusca, '$options': 'i'}
+                }, {
+                    cidade: {'$regex': data.local, '$options': 'i'}
+                }, {
+                    estado: {'$regex': data.local, '$options': 'i'}
+                }, {
+                    bairro: {'$regex': data.local, '$options': 'i'}
+                }]
             }, {
-                cidade: {'$regex': data.local, '$options': 'i'}
+                nomeEstudio: 1,
+                cidade: 1,
+                estado:1,
+                bairro: 1
+            }, function(erro, resultado){
+                if(resultado){
+                    resolve({status : true, 'resultado': resultado});
+                }else{
+                    reject({status :false, erro: erro});
+                }
+            });
+        } else if(data.nomeBusca != "" && data.local == "") {
+            EstudioModel.find({
+                "$or": [{
+                    nomeEstudio: {'$regex': data.nomeBusca, '$options': 'i'}
+                }]
             }, {
-                estado: {'$regex': data.local, '$options': 'i'}
+                nomeEstudio: 1,
+                cidade: 1,
+                estado:1,
+                bairro: 1
+            }, function(erro, resultado){
+                if(resultado){
+                    resolve({status : true, 'resultado': resultado});
+                }else{
+                    reject({status :false, erro: erro});
+                }
+            });
+        } else if(data.nomeBusca == "" && data.local != "") {
+            EstudioModel.find({
+                "$or": [{
+                    cidade: {'$regex': data.local, '$options': 'i'}
+                }, {
+                    estado: {'$regex': data.local, '$options': 'i'}
+                }, {
+                    bairro: {'$regex': data.local, '$options': 'i'}
+                }]
             }, {
-                bairro: {'$regex': data.local, '$options': 'i'}
-            }]
-        }, {
-            nomeEstudio: 1,
-            cidade: 1,
-            estado:1,
-            bairro: 1
-        }, function(erro, resultado){
-            if(resultado){
-                resolve({status : true, 'resultado': resultado});
-            }else{
-                reject({status :false, erro: erro});
-            }
-        })
+                nomeEstudio: 1,
+                cidade: 1,
+                estado:1,
+                bairro: 1
+            }, function(erro, resultado){
+                if(resultado){
+                    resolve({status : true, 'resultado': resultado});
+                }else{
+                    reject({status :false, erro: erro});
+                }
+            });
+        }
     });
 }
