81cbc0a7b407b1c72b0406f3558aca85dc737f12
Felipe Chaves <felipechaves14@bol.com.br>
2018-08-28 21:45:38 -0300

    Busca quase la

--- routes/buscarRoute.js
+++ routes/buscarRoute.js
@@ -2,24 +2,18 @@ var express = require('express');
 let ServicoBusiness = require('../business/servicoBusiness');
 var router = express.Router();
 
-router.get('/listar-estudios', function(req, res, next) {
-    res.render('listarEstudios');
-});
+router.get('/listar-estudios', function(req, res, next){
 
-router.post('/listar-estudios', function(req, res, next){
-    console.log("COMECOU!!");
-    let nomeBusca = req.body.nome;
-    let local = req.body.local;
+    let nomeBusca = req.query.nome;
+    let local = req.query.local;
     let busca= {
         "nomeBusca": nomeBusca,
         "local": local
     };
 
-    console.log(nomeBusca);
-    console.log(local);
 
     ServicoBusiness.buscar(busca).then(function(objeto){
-        console.log("Deu 'sucesso'");
+        console.log(objeto);
        res.render('listarEstudios', {resultado: objeto});
     }).catch(function(erro){
         res.end(JSON.stringify(erro));
