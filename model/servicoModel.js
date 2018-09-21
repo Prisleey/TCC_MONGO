8d94ad34058a561888bb49f82c06c335a7dc62c1
Felipe Chaves de Oliveira <felipechaves14@bol.com.br>
2018-09-20 22:37:35 -0300

    vou jogar fora isso

--- model/servicoModel.js
+++ model/servicoModel.js
@@ -5,6 +5,8 @@ let servicoSchema = new Schema({
     nomeServico: {type: String},
     descricaoServico: {type: String},
     precoServico: {type: Number, required: true},
+    taxaDesconto: {type: Number, required: true},
+    valorComDesconto: {type: Number, required: true},
     idTipoServico: {type: Schema.Types.ObjectId, ref: 'TipoServico'},
     idSala: {type: Schema.Types.ObjectId, ref: 'Estudio.salas'},
 }, {collection : 'Servico'});
