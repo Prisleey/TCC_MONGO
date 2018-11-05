df56cc1c9c3c1c5881e4c08bb85c3e8f858646d2
Felipe Chaves de Oliveira <felipechaves14@bol.com.br>
2018-11-05 19:14:21 -0200

    carteira no tipo

--- routes/carteiraRoute.js
+++ routes/carteiraRoute.js
@@ -5,9 +5,10 @@ let CarteiraBusiness = require('../business/carteiraBusiness');
 router.get('/carteira', function(req, res, next) {
     let id_user = req.session.usuarioLogado[0]._id;
 
-    CarteiraBusiness.consultarAgendamento(id_user).then(function(agendamentos){
-        console.log(agendamentos);
-        res.render('agenda', {usuarioLogado: req.session.usuarioLogado});
+    CarteiraBusiness.consultarCarteira(id_user).then(function(carteira){
+        console.log("EITA");
+        console.log(carteira.carteira[0].creditos);
+        res.render('carteira', {"creditos": carteira.carteira[0].creditos, usuarioLogado: req.session.usuarioLogado});
     }).catch(function(erro) {
         res.end(erro);
     });
