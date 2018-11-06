3a77eb06f454408721269ce553aec505e0bbb53d
Felipe Chaves de Oliveira <felipechaves14@bol.com.br>
2018-11-06 04:21:27 -0200

    inicio do pagamento com creditos

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -2,10 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 let UsuarioBusiness = require('../business/usuarioBusiness');
+var CarteiraBusiness = require('../business/carteiraBusiness');
 
 router.get('/', function(req, res, next) {
     UsuarioBusiness.listTipoUsuario().then(function(objeto) {
-        res.render('index', { tiposUser: objeto.tipos, usuarioLogado: req.session.usuarioLogado});
+        if(typeof req.session.usuarioLogado !== 'undefined' && req.session.usuarioLogado.length > 0){
+            CarteiraBusiness.consultarCarteira(req.session.usuarioLogado[0]._id).then(function(carteira){
+                res.render('index', {tiposUser: objeto.tipos, usuarioLogado: req.session.usuarioLogado, creditos: carteira.carteira[0].creditos});
+            }).catch(function(error){
+                res.end(JSON.stringify(error));
+            });
+        }else {
+            res.render('index', {tiposUser: objeto.tipos, usuarioLogado: req.session.usuarioLogado});
+        }
     });
 });
 
